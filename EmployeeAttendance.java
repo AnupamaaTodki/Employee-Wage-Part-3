5ed62fd715a51c10b05d7d99bf5e24a66cbf1888
AnupamaaTodki <todkianupama@gmail.com>
2022-08-30 23:15:19 +0530

    ADDED: Employee attendance File added to remote code base

---
+++ EmployeeAttendance.java
@@ -0,0 +1,22 @@
+package com.employeewage3;
+
+public class EmployeeAttendance {
+	
+	public int IS_FULL_TIME = 1;
+	
+	public void getAttendance() {
+
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+		   System.out.println("Employee is present");
+		else
+		   System.out.println("Employee is absent");
+		}
+
+
+public static void main(String[] args) {
+EmployeeAttendance emp = new EmployeeAttendance();
+emp.getAttendance();
+}
+
+}
